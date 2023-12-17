80c540ed7f03b73800dc854a2f53d03a8a012e48
faith rono <faithrono132@gmail.com>
2023-12-17 04:14:51 -0800

    slection sorting

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,44 +1,41 @@
 #include "sort.h"
 
 /**
- * selection_sort - Sorts an array of integers in ascending order
+ * selection_sort - function that sorts integers in ascending order
  *                  using the Selection sort algorithm
- * @array: Pointer to the first element of the array
- * @size: Number of elements in the array
+ * @array: Pointer to the first element of the array of integers
+ * @size: size of integers in an array being sorted
  */
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, min_idx;
+	size_t i, j, smallest_value;
 
-    if (array == NULL || size < 2)
-        return;
-
-    for (i = 0; i < size - 1; i++)
-    {
-        min_idx = i;
-
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[min_idx])
-                min_idx = j;
-        }
-
-        if (min_idx != i)
-        {
-            swap(&array[i], &array[min_idx]);
-            print_array(array, size);
-        }
-    }
+	if (array == NULL || size < 2)
+		return;
+	for (i = 0; i < size - 1; i++)
+	{
+		smallest_value = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[smallest_value])
+				smallest_value = j;
+		}
+		if (min_idx != i)
+		{
+			swap(&array[i], &array[smallest_value]);
+			print_array(array, size);
+		}
+	}
 }
 
 /**
- * swap - Swaps two integers
- * @a: Pointer to the first integer
- * @b: Pointer to the second integer
+ * swap - a function that swaps the selected values of two integers
+ * @first: Pointer to the selected first integer
+ * @second: Pointer to the selected second integer
  */
-void swap(int *a, int *b)
+void swap(int *first, int *second)
 {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+	int store = *first;
+	*first = *second;
+	*second = store;
 }
