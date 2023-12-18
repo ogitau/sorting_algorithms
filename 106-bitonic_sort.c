6f11fc65282325303e68e229fd606aa488a9fc3e
Owen Gitau <owengitau02@gmail.com>
2023-12-18 14:02:51 -0800

    Bitonic sorting

--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -2,52 +2,48 @@
 #include <stdio.h>
 
 /**
- * bitonic_compare - helper to bitonic_merge, sorts contents of current
- * subarray
- * @ascending: if true, sort in ascending order, false, descending
+ * bitonic_compare_ascending - helper to bitonic_merge
  * @x: subarray in current frame of recursion
  * @size: number of elements in `x`
  */
 void bitonic_compare_ascending(int *x, size_t size)
 {
-    size_t dist, idx;
-    int temp;
+	size_t dist, idx;
+	int temp;
 
-    dist = size / 2;
-    for (idx = 0; idx < dist; idx++)
-    {
-        if (x[idx] > x[idx + dist])
-        {
-            temp = x[idx];
-            x[idx] = x[idx + dist];
-            x[idx + dist] = temp;
-        }
-    }
+	dist = size / 2;
+	for (idx = 0; idx < dist; idx++)
+	{
+		if (x[idx] > x[idx + dist])
+		{
+		temp = x[idx];
+		x[idx] = x[idx + dist];
+		x[idx + dist] = temp;
+		}
+	}
 }
 
 /**
- * bitonic_compare_descending - helper to bitonic_merge, sorts contents of current
- * subarray in descending order
+ * bitonic_compare_descending - helper to bitonic_merge
  * @x: subarray in current frame of recursion
  * @size: number of elements in `x`
  */
 void bitonic_compare_descending(int *x, size_t size)
 {
-    size_t dist, idx;
-    int temp;
+	size_t dist, idx;
+	int temp;
 
-    dist = size / 2;
-    for (idx = 0; idx < dist; idx++)
-    {
-        if (x[idx] < x[idx + dist])
-        {
-            temp = x[idx];
-            x[idx] = x[idx + dist];
-            x[idx + dist] = temp;
-        }
-    }
+	dist = size / 2;
+	for (idx = 0; idx < dist; idx++)
+	{
+		if (x[idx] < x[idx + dist])
+		{
+		temp = x[idx];
+		x[idx] = x[idx + dist];
+		x[idx + dist] = temp;
+		}
+	}
 }
-
 /**
  * bitonic_merge - second recursive function of bitonic_sort
  * @x: subarray in previous frame of recursion
@@ -56,16 +52,16 @@ void bitonic_compare_descending(int *x, size_t size)
  */
 void bitonic_merge(int *x, size_t size, size_t orig_size)
 {
-    int *first, *second;
+	int *first, *second;
 
-    if (size > 1)
-    {
-        first = x;
-        second = x + (size / 2);
-        bitonic_compare_ascending(x, size);
-        bitonic_merge(first, size / 2, orig_size);
-        bitonic_merge(second, size / 2, orig_size);
-    }
+	if (size > 1)
+	{
+		first = x;
+		second = x + (size / 2);
+		bitonic_compare_ascending(x, size);
+		bitonic_merge(first, size / 2, orig_size);
+		bitonic_merge(second, size / 2, orig_size);
+	}
 }
 
 /**
@@ -76,19 +72,20 @@ void bitonic_merge(int *x, size_t size, size_t orig_size)
  */
 void bitonic_sort_r(int *x, size_t size, size_t orig_size)
 {
-    int *first, *second;
+	int *first, *second;
 
-    if (size <= 1)
-        return;
-    first = x;
-    second = x + (size / 2);
-    printf("Merging [%lu/%lu] (UP):\n", size, orig_size);
-    print_array(x, size);
-    bitonic_sort_r(first, size / 2, orig_size);
-    bitonic_sort_r(second, size / 2, orig_size);
-    bitonic_merge(x, size, orig_size);
-    printf("Result [%lu/%lu] (UP):\n", size, orig_size);
-    print_array(x, size);
+	if (size <= 1)
+		return;
+
+	first = x;
+	second = x + (size / 2);
+	printf("Merging [%lu/%lu] (UP):\n", size, orig_size);
+	print_array(x, size);
+	bitonic_sort_r(first, size / 2, orig_size);
+	bitonic_sort_r(second, size / 2, orig_size);
+	bitonic_merge(x, size, orig_size);
+	printf("Result [%lu/%lu] (UP):\n", size, orig_size);
+	print_array(x, size);
 }
 
 /**
@@ -99,9 +96,8 @@ void bitonic_sort_r(int *x, size_t size, size_t orig_size)
  */
 void bitonic_sort(int *array, size_t size)
 {
-    if (!array || size == 0)
-        return;
-
-    bitonic_sort_r(array, size, size);
+	if (!array || size == 0)
+		return;
+	bitonic_sort_r(array, size, size);
 }
 
