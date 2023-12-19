7bf17a46ae4c0f047348ff887f85ece64c4c8e9e
faith rono <faithrono132@gmail.com>
2023-12-19 14:30:50 -0800

    shell sorting

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -8,22 +8,29 @@
  */
 void shell_sort(int *array, size_t size)
 {
-	size_t gap = 1, k, m;
-	int store;
-
-	if (array == NULL || size < 2)
+	int inner_loop = 0;
+	int outer_loop = 0;
+	int gap = 1;
+	int swapper = 0;
+	
+	if (!array || size < 2)
 		return;
-	while (gap < size / 3)
+	while (gap < (int)size / 3)
 		gap = gap * 3 + 1;
 	while (gap > 0)
 	{
-		for (k = gap; k < size; k++)
+		outer_loop = gap;
+		while (outer_loop < (int)size)
 		{
-			store = array[k];
-			for (m = k; m >= gap && array[m  - gap] > store; /
-					m -= gap)
-				array[m] = array[m - gap];
-			array[m] = store;
+			swapper = array[outer_loop];
+			inner_loop = outer_loop;
+			while (inner_loop > gap - 1 && array[inner_loop - gap] >= swapper)
+			{
+				array[inner_loop] = array[inner_loop - gap];
+				inner_loop = inner_loop - gap;
+			}
+			array[inner_loop] = swapper;
+			outer_loop++;
 		}
 		print_array(array, size);
 		gap = (gap - 1) / 3;
